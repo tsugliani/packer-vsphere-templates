6efffe606b03e4794d88aec084b350d2ad4d4fad
Timo Sugliani <timo.sugliani@gmail.com>
2016-05-18 11:59:07 +0200

    indentation fixes

--- scripts/Linux/CentOS/cleanup.sh
+++ scripts/Linux/CentOS/cleanup.sh
@@ -43,29 +43,29 @@ FileSystem=`grep ext /etc/mtab | awk -F" " '{ print $2 }'`
 
 for i in $FileSystem
 do
-        echo $i
-        number=`df -B 512 $i | awk -F" " '{print $3}' | grep -v Used`
-        echo $number
-        percent=$(echo "scale=0; $number * 98 / 100" | bc )
-        echo $percent
-        dd count=`echo $percent` if=/dev/zero of=`echo $i`/zf
-        /bin/sync
-        sleep 15
-        rm -f $i/zf
+  echo $i
+  number=`df -B 512 $i | awk -F" " '{print $3}' | grep -v Used`
+  echo $number
+  percent=$(echo "scale=0; $number * 98 / 100" | bc )
+  echo $percent
+  dd count=`echo $percent` if=/dev/zero of=`echo $i`/zf
+  /bin/sync
+  sleep 15
+  rm -f $i/zf
 done
 
 VolumeGroup=`vgdisplay | grep Name | awk -F" " '{ print $3 }'`
 
 for j in $VolumeGroup
 do
-        echo $j
-        lvcreate -l `vgdisplay $j | grep Free | awk -F" " '{ print $5 }'` -n zero $j
-        if [ -a /dev/$j/zero ]; then
-                cat /dev/zero > /dev/$j/zero
-                /bin/sync
-                sleep 15
-                lvremove -f /dev/$j/zero
-        fi
+  echo $j
+  lvcreate -l `vgdisplay $j | grep Free | awk -F" " '{ print $5 }'` -n zero $j
+  if [ -a /dev/$j/zero ]; then
+    cat /dev/zero > /dev/$j/zero
+    /bin/sync
+    sleep 15
+    lvremove -f /dev/$j/zero
+  fi
 done
 
 echo "==> Zero out the free space to save space in the final image"
